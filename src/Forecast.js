fbbca8857a0750e557f1e9155ef6c51dcfd44a0a
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-06-22 11:09:32 +0200

    Refined css

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,10 +1,48 @@
 import React from "react";
+import ReactAnimatedWeather from 'react-animated-weather';
+import './Forecast.css';
 
 export default function Forecast() {
   return (
     <div className="Forecast">
       <div class="row">
-        <p>Here comes the forecast!!</p>
+          <ul>
+          <li id="icon">
+ <ReactAnimatedWeather
+        icon="CLEAR_DAY"
+        color="grey"
+        size={52}
+        animate="true"
+      /></li>
+       <li id="icon">
+      <ReactAnimatedWeather
+        icon="CLOUDY"
+        color="grey"
+        size={52}
+        animate="true"
+      /></li>
+       <li id="icon">
+      <ReactAnimatedWeather
+        icon="WIND"
+        color="grey"
+        size={52}
+        animate="true"
+      /></li>
+       <li id="icon">
+      <ReactAnimatedWeather
+        icon="FOG"
+        color="grey"
+        size={52}
+        animate="true"
+      /></li>
+       <li id="icon">
+      <ReactAnimatedWeather
+        icon="SNOW"
+        color="grey"
+        size={52}
+        animate="true"
+      /></li>
+</ul>
       </div>
     </div>
   );
