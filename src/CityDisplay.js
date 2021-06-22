4968d6e6bf3f7033f1c7d5c59433ffc16eb1894f
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-06-22 10:18:03 +0200

    Created CityDisplay js and css

---
+++ src/CityDisplay.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CityDisplay() {
+  return (
+    <div className="CityDisplay">
+      <div class="row">
+        <div class="col">
+          <h1 id="city">Valencia</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
