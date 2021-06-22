6b4e552e6cba66b243be8509db53963e69d47627
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-06-22 10:23:45 +0200

    Created Forecast js and css

---
+++ src/Forecast.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Forecast() {
+  return (
+    <div className="Forecast">
+      <div class="row">
+        <p>Here comes the forecast!!</p>
+      </div>
+    </div>
+  );
+}
