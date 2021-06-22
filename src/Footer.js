e80b901054193954c7725070308840373e4980a1
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-06-22 10:20:03 +0200

    Created Footer js and css

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p class="coded-by">
+        <a href="https://github.com/tania-garcia-gallego">
+          Open source code
+        </a>
+        by Tania Garcia Gallego 👩‍💻
+      </p>
+    </div>
+  );
+}
